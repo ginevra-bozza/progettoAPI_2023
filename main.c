091a9d1e41221d156f45a32820e683ae42727039
Ginevra Bozza <ginevra.bozza@mail.polimi.it>
2023-11-22 16:22:13 +0100

    fixed addStation and addCar

--- main.c
+++ main.c
@@ -9,26 +9,29 @@
 #define BESTROUTE "pianifica-percorso"
 #define LEN 19 //max length of input + 1
 
-typedef struct parkingBST_s{
-    struct parkingBST_s *left, *right;
-    int fuel;
-} parkingBST_t;
+typedef struct carBST_s{
+    struct carBST_s *left, *right;
+    int autonomy;
+    int carCounter;
+} carBST_t;
 
 typedef struct statBST_s{
     struct statBST_s *left, *right;
     int distance;
     int carCount;
-    parkingBST_t *parking;
+    carBST_t *parking;
 } statBST_t;
 
 
 int stringCmp(const char* , const char* );
 
 statBST_t *addStation(statBST_t **, int);
-int addCar(int, int);
+int addCar(carBST_t **, int);
 int removeStation(statBST_t *, int);
 int removeCar(int, int);
 void planRoute(int, int);
+
+statBST_t *checkStation(int distance);
 
 int main(){
     statBST_t *sRoot = NULL; //root of the BST containing all stations
@@ -39,7 +42,7 @@ int main(){
 
     statBST_t *station = NULL;
     statBST_t  *addedStation = NULL;
-    parkingBST_t * carParking = NULL;
+    carBST_t * carParking = NULL;
 
     //creating root of station BST
     sRoot = malloc(sizeof (statBST_t));
@@ -63,79 +66,92 @@ int main(){
     }
     strtok(input, "\n"); //input string has an extra \n at the end
      */
-    scanf("%s",input);
 
-    // while(!feof(stdin)){
-    if(strcmp(input, ADDSTAT) == 0){
-        printf("ADDSTAT\n");
+    while(!feof(stdin)) {
+        scanf("%s",input);
+        if (strcmp(input, ADDSTAT) == 0) {
+            printf("ADDSTAT\n");
 
-        scanf("%d",&distance);
-        addedStation = addStation(&sRoot, distance); //addStation return a pointer to the newly added station if added, else return NULL
+            scanf("%d", &distance);
+            addedStation = addStation(&sRoot,
+                                      distance); //addStation return a pointer to the newly added station if added, else return NULL
 
-        if (addedStation != NULL){
-            scanf("%d",&numOfCars);
-            printf("Num of cars: %d",numOfCars);
-            addedStation -> carCount = numOfCars;
+            if (addedStation != NULL) {
+                scanf("%d", &numOfCars);
+                printf("Num of cars: %d", numOfCars);
+                addedStation->carCount = numOfCars;
 
-            carParking = malloc(sizeof (parkingBST_t));
-            if(carParking == NULL){
-                printf("Memory allocation error - new parking");
-                return 1;
+                carParking = malloc(sizeof(carBST_t));
+                if (carParking == NULL) {
+                    printf("Memory allocation error - new parking");
+                    return 1;
+                }
+                for (int i = 1; i <= numOfCars; i++) {
+                    scanf("%d", &carAutonomy);
+                    addCar(&addedStation->parking, carAutonomy);
+                }
+                printf("aggiunta");
+            } else {
+                printf("non aggiunta");
+                free(station);
             }
-            for(int i = 0; i <= numOfCars; i++){
-                scanf("%d",&carAutonomy);
-                addCar(addedStation->distance, carAutonomy);
+
+        } else if (strcmp(input, REMOVESTAT) == 0) {
+            printf("REMOVESTAT");
+
+            scanf("%d", &distance);
+            if (distance > 0) {
+                if (removeStation(sRoot, distance)) {
+                    printf("demolita");
+                } else {
+                    printf("non demolita");
+                }
+            } else {
+                printf("Error in removal - invalid distance");
             }
-            printf("aggiunta");
-        } else {
-            printf("non aggiunta");
-            free(station);
-        }
 
-    } else if(strcmp(input, REMOVESTAT) == 0){
-        printf("REMOVESTAT");
+        } else if (strcmp(input, ADDCAR) == 0) {
+            printf("ADDCAR");
+            scanf("%d", &distance);
+            scanf("%d", &carAutonomy);
 
-        scanf("%d",&distance);
-        if(distance > 0){
-            if(removeStation(sRoot,distance)){
+            station = checkStation(distance);
+            if(station != NULL){
+                addCar((carBST_t **) &station->parking, carAutonomy);
+
+            } else {
+                printf("non aggiunta");
+            }
+
+
+
+            if (addCar(NULL, carAutonomy)) {
+                printf("aggiunta");
+            } else {
+                printf("non aggiunta");
+            }
+
+        } else if (strcmp(input, REMOVECAR) == 0) {
+            printf("REMOVECAR");
+            scanf("%d", &distance);
+            scanf("%d", &carAutonomy);
+            if (removeCar(distance, carAutonomy)) {
                 printf("demolita");
             } else {
                 printf("non demolita");
             }
+
+        } else if (strcmp(input, BESTROUTE) == 0) {
+            printf("BESTROUTE");
+            int start;
+            int end;
+            scanf("%d", &start);
+            scanf("%d", &end);
+            planRoute(start, end);
+
         } else {
-            printf("Error in removal - invalid distance");
+            printf("aaaaaaa");
         }
-
-    } else if(strcmp(input, ADDCAR) == 0){
-        printf("ADDCAR");
-        scanf("%d", &distance);
-        scanf("%d",&carAutonomy);
-        if(addCar(distance, carAutonomy)){
-            printf("aggiunta");
-        } else {
-            printf("non aggiunta");
-        }
-
-    } else if(strcmp(input, REMOVECAR) == 0){
-        printf("REMOVECAR");
-        scanf("%d", &distance);
-        scanf("%d",&carAutonomy);
-        if(removeCar(distance, carAutonomy)){
-            printf("demolita");
-        } else {
-            printf("non demolita");
-        }
-
-    } else if(strcmp(input,BESTROUTE) == 0){
-        printf("BESTROUTE");
-        int start;
-        int end;
-        scanf("%d",&start);
-        scanf("%d",&end);
-        planRoute(start,end);
-
-    } else {
-        printf("aaaaaaa");
     }
 
     /*if(fgets(input, LEN, stdin) == NULL){
@@ -147,16 +163,19 @@ int main(){
 
 }
 
-statBST_t * addStation(statBST_t ** root, int dist) {
-    statBST_t* current = *root;
-    statBST_t* parent = NULL;
+statBST_t *checkStation(int distance) {
+    return NULL;
+}
+
+statBST_t * addStation(statBST_t ** root, int dist){
+    statBST_t *current = *root;
+    statBST_t *parent = NULL;
     statBST_t *station = NULL;
 
-    // Traverse the tree to find the appropriate position for the new node
     while (current != NULL) {
         if (dist == current->distance) {
-            // If the dist already exists in the tree, return NULL
-            return NULL;
+            return NULL;// if the dist is already present in the tree, return NULL
+
         } else if (dist < current->distance) {
             parent = current;
             current = current->left;
@@ -176,6 +195,7 @@ statBST_t * addStation(statBST_t ** root, int dist) {
     station->left = NULL;
     station->parking = NULL;
 
+    //insert the station in the tree
     if(dist < parent->distance)
         parent->left = station;
     else
@@ -190,8 +210,58 @@ statBST_t * addStation(statBST_t ** root, int dist) {
 int removeStation(statBST_t *pS, int distance) {
     return 0;
 }
-int addCar(int distance, int autonomy) {
+int addCar(carBST_t ** root, int autonomy) {
 
+    carBST_t *current = *root;
+    carBST_t *parent = NULL;
+    carBST_t *car = NULL;
+
+    if(current == NULL){
+        car = malloc(sizeof (carBST_t ));
+        if(car == NULL){
+            printf("Memory allocation error - new car");
+            return 0;
+        }
+        car->carCounter = 1;
+        car->autonomy = autonomy;
+        car->right = NULL;
+        car->left = NULL;
+
+        *root = car;
+        return 1;
+    }
+
+    while (current != NULL) {
+        if (autonomy == current->autonomy) {
+            current->carCounter++;
+            return 1;
+        } else if (autonomy < current->autonomy) {
+            parent = current;
+            current = current->left;
+        } else {
+            parent = current;
+            current = current->right;
+        }
+    }
+
+    car = malloc(sizeof (carBST_t ));
+    if(car == NULL){
+        printf("Memory allocation error - new car");
+        return 0;
+    }
+    car->carCounter = 1;
+    car->autonomy = autonomy;
+    car->right = NULL;
+    car->left = NULL;
+
+    //insert the car in the tree
+
+    if(autonomy < parent->autonomy)
+        parent->left = car;
+    else
+        parent->right = car;
+
+    return 1;
 }
 int removeCar(int distance, int autonomy) {
     return 0;
