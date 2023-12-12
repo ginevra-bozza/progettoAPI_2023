f8d3c5ee13f801390bff17861b29531439f7fbbd
Ginevra Bozza <ginevra.bozza@mail.polimi.it>
2023-12-12 15:56:58 +0100

    changes to cmake file

---
+++ cmake-build-debug/CMakeFiles/progettoAPI_2023.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progettoAPI_2023.dir/main.c.obj"
+  "CMakeFiles/progettoAPI_2023.dir/main.c.obj.d"
+  "libprogettoAPI_2023.dll.a"
+  "progettoAPI_2023.exe"
+  "progettoAPI_2023.exe.manifest"
+  "progettoAPI_2023.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/progettoAPI_2023.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/progettoAPI_2023.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for progettoAPI_2023.
+# This may be replaced when dependencies are built.
